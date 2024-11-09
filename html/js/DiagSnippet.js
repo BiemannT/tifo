029d227e5ac256a8d957bda171c76c7e4333bfc0
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2024-11-09 10:52:31 +0100

    Update DiagSnippet.js
    Added functionality to Setup language versions in the snippet dialog.

--- html/js/DiagSnippet.js
+++ html/js/DiagSnippet.js
@@ -5,6 +5,7 @@ const fieldStruct = diagForm.querySelector("fieldset:nth-of-type(2)");
 const numStruct = fieldStruct.querySelector("legend input[type='number']");
 const btnIncStruct = fieldStruct.querySelector("div.DiagButton:nth-of-type(1)");
 const btnDecStruct = fieldStruct.querySelector("div.DiagButton:nth-of-type(2)");
+const fieldVersion = diagForm.querySelector("fieldset:nth-of-type(3)");
 
 function ShowDiagSnippet(tifoData) {
     // Try to fetch tifo data and display the content
@@ -20,6 +21,88 @@ function ShowDiagSnippet(tifoData) {
         document.getElementById(`DiagEditSnipStructLvl${i}`).value = StructInfo[i - 1].textContent;
     }
 
+    // Load Versions
+    // Cleanup existing Versions, if dialog was opened before
+    const OldVersions = fieldVersion.querySelectorAll("section");
+    for (let i = 0; i < OldVersions.length; i++) {
+        fieldVersion.removeChild(OldVersions[i]);
+    }
+
+    // Iterate all available versions
+    const tifoVersions = tifoData.querySelectorAll("version");
+    for (let i = 0; i < tifoVersions.length; i++) {
+        
+        const VersionNumber = tifoVersions[i].getAttribute("number");
+        let sect = document.createElement("section");
+        let header = document.createElement("h1");
+        let list = document.createElement("ul");
+
+        header.textContent = `Version ${VersionNumber}`;
+
+        // Iterate all available languages
+        let tifoVersionsLangauges = tifoVersions[i].querySelectorAll("content");
+        for (let j = 0; j < tifoVersionsLangauges.length; j++) {
+            let listEntry = document.createElement("li");
+            let EntryLabel = document.createElement("label");
+            let EntryLabelLang = document.createElement("data");
+            let EntryLabelTranstate = document.createElement("data");
+            let EntryLabelDate = document.createElement("time");
+            let EntryText = document.createElement("textarea");
+
+            const TranslateLang = tifoVersionsLangauges[j].getAttribute("lang");
+            const TranslateDate = new Date(tifoVersionsLangauges[j].getAttribute("date"));
+            const TranslateState = tifoVersionsLangauges[j].getAttribute("transtate");
+
+            EntryLabelLang.value = TranslateLang;
+            EntryLabelLang.textContent = TranslateLang;
+
+            EntryLabelTranstate.value = TranslateState;
+            switch (TranslateState) {
+                case "original":
+                    EntryLabelTranstate.textContent = "Original";
+                    break;
+            
+                case "pending":
+                    EntryLabelTranstate.textContent = "Übersetzung ausstehend";
+                    break;
+
+                case "translated":
+                    EntryLabelTranstate.textContent = "Übersetzung";
+                    break;
+
+                default:
+                    break;
+            }
+
+            EntryLabelDate.dateTime = tifoVersionsLangauges[j].getAttribute("date");
+            EntryLabelDate.textContent = TranslateDate.toLocaleDateString();
+
+            EntryLabel.htmlFor = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
+            EntryLabel.appendChild(EntryLabelLang);
+            EntryLabel.appendChild(EntryLabelTranstate);
+            EntryLabel.appendChild(EntryLabelDate);
+
+            EntryText.name = `Version${VersionNumber}${TranslateLang}`;
+            EntryText.id = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
+            EntryText.wrap = "soft";
+            EntryText.required = true;
+            EntryText.innerHTML = tifoVersionsLangauges[j].innerHTML;
+
+            if (j > 0) {
+                // Set readonly for older Versions, if available
+                EntryText.readOnly = true;
+            }
+
+            listEntry.appendChild(EntryLabel);
+            listEntry.appendChild(EntryText);
+            list.appendChild(listEntry);
+        }
+
+        sect.appendChild(header);
+        sect.appendChild(list);
+        fieldVersion.appendChild(sect);
+    }
+
     diagSnip.showModal();
 }
 
@@ -33,7 +116,6 @@ btnClose.addEventListener("click", () => {
 // Increase-Button
 btnIncStruct.addEventListener("click", () => {
     diagForm.StructLvlCount.stepUp();
-    // AddStructLvl();
     SetupStructureLevels();
 });
 
@@ -41,7 +123,6 @@ btnIncStruct.addEventListener("click", () => {
 btnDecStruct.addEventListener("click", () => {
     if (diagForm.StructLvlCount.value > 0) {
         diagForm.StructLvlCount.stepDown();
-        // RemStructLvl();
         SetupStructureLevels();
     }
 });
