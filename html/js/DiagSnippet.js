a9b519a248c628276571ee6555510eea88df1b0f
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2024-11-09 18:23:17 +0100

    Update DiagSnippet.js
    Arrange existing code in a class design.

--- html/js/DiagSnippet.js
+++ html/js/DiagSnippet.js
@@ -1,173 +1,209 @@
-const diagSnip = document.getElementById("DiagEditSnippet");
-const diagForm = diagSnip.querySelector("form");
-const btnClose = diagSnip.querySelector("header div.DiagClose");
-const fieldStruct = diagForm.querySelector("fieldset:nth-of-type(2)");
-const numStruct = fieldStruct.querySelector("legend input[type='number']");
-const btnIncStruct = fieldStruct.querySelector("div.DiagButton:nth-of-type(1)");
-const btnDecStruct = fieldStruct.querySelector("div.DiagButton:nth-of-type(2)");
-const fieldVersion = diagForm.querySelector("fieldset:nth-of-type(3)");
-
-function ShowDiagSnippet(tifoData) {
-    // Try to fetch tifo data and display the content
-    diagForm.Description.value = tifoData.querySelector("info description").textContent;
-    diagForm.Guid.value = tifoData.getAttribute("guid");
+class DialogSnippets {
+    #diagSnip;
+    #diagForm;
     
-    // Load Structure Info
-    const StructInfo = tifoData.querySelectorAll("info structure level");
-    numStruct.value = StructInfo.length;
-    SetupStructureLevels();
+    #btnClose;
+    #btnIncStruct;
+    #btnDecStruct;
+    #btnNewVersion;
 
-    for (let i = 1; i <= numStruct.value; i++) {
-        document.getElementById(`DiagEditSnipStructLvl${i}`).value = StructInfo[i - 1].textContent;
+    #fieldStruct;
+    #fieldVersion;
+
+    #numStruct;
+    
+    /**
+     * Initialize the logic for the Snippet Dialog.
+     * @param {string} DialogID HTML Document ID of the Snippet Dialog.
+     */
+    constructor(DialogID) {
+        // Setup local vars
+        this.#diagSnip = document.getElementById(DialogID);
+        this.#diagForm = this.#diagSnip.querySelector("form");
+        this.#btnClose = this.#diagSnip.querySelector("header div.DiagClose");
+        this.#fieldStruct = this.#diagForm.querySelector("fieldset:nth-of-type(2)");
+        this.#numStruct = this.#fieldStruct.querySelector("legend input[type='number']");
+        this.#btnIncStruct = this.#fieldStruct.querySelector("div.DiagButton:nth-of-type(1)");
+        this.#btnDecStruct = this.#fieldStruct.querySelector("div.DiagButton:nth-of-type(2)");
+        this.#fieldVersion = this.#diagForm.querySelector("fieldset:nth-of-type(3)");
+        this.#btnNewVersion = this.#fieldVersion.querySelector("div.DiagButton:nth-of-type(1)");
+
+        // Setup Event Handler for Buttons
+        // Logic for close dialog button
+        this.#btnClose.addEventListener("click", () => {
+            this.#diagForm.reset();
+            this.#diagSnip.close();
+        });
+
+        // Logic for increasing and decreasing structure levels
+        // Increase-Button
+        this.#btnIncStruct.addEventListener("click", () => {
+            this.#diagForm.StructLvlCount.stepUp();
+            SetupStructureLevels();
+        });
+
+        // Decrease-Button
+        this.#btnDecStruct.addEventListener("click", () => {
+            if (this.#diagForm.StructLvlCount.value > 0) {
+                this.#diagForm.StructLvlCount.stepDown();
+                SetupStructureLevels();
+            }
+        });
+
+        // Event-Handler for changing Structure list
+        this.#numStruct.addEventListener("change", () => {
+            SetupStructureLevels();
+        });
+
     }
 
-    // Load Versions
-    // Cleanup existing Versions, if dialog was opened before
-    const OldVersions = fieldVersion.querySelectorAll("section");
-    for (let i = 0; i < OldVersions.length; i++) {
-        fieldVersion.removeChild(OldVersions[i]);
+    /**
+     * This private method setup the visual content of the Snippet Structure viewbox.
+     * The visible levels will be adapted to the #numStruct-value.
+     */
+    #SetupStructureLevels() {
+        const NewNumberLevels = Number(this.#numStruct.value);
+        const ActualNumberLevels = this.#fieldStruct.querySelectorAll("div.DiagStructLevel").length;
+
+        if (NewNumberLevels > ActualNumberLevels) {
+            // Adding New Structure Levels
+            for (let i = ActualNumberLevels + 1; i <= NewNumberLevels; i++) {
+                let div = document.createElement("div");
+                let label = document.createElement("label");
+                let input = document.createElement("input");
+
+                label.setAttribute("for", `DiagEditSnipStructLvl${i}`);
+                label.textContent = `Ebene ${i}:`;
+
+                input.type = "text";
+                input.name = `StructLvl${i}`;
+                input.id = `DiagEditSnipStructLvl${i}`;
+                input.placeholder = `Name Ebene ${i}...`;
+                input.minLength = "2";
+                input.maxLength = "20";
+                input.required = true;
+
+                div.className = "DiagStructLevel";
+                div.appendChild(label);
+                div.appendChild(input);
+
+                this.#diagForm.querySelector("fieldset:nth-of-type(2)").appendChild(div);
+                
+            }
+        }
+
+        if (NewNumberLevels < ActualNumberLevels) {
+            // Remove last Structure Levels
+            for (let i = ActualNumberLevels; i > NewNumberLevels; i--) {
+                const lstStruct = this.#fieldStruct.querySelector("div.DiagStructLevel:last-of-type");
+                fieldStruct.removeChild(lstStruct);
+            }
+        }
     }
 
-    // Iterate all available versions
-    const tifoVersions = tifoData.querySelectorAll("version");
-    for (let i = 0; i < tifoVersions.length; i++) {
+    /**
+     * Show the content of an tifo-Block in the Snippet Dialog.
+     * @param {Element} tifoData Reference to the full tifo-Block, which content should be displayed.
+     */
+    ShowDiag(tifoData) {
+        // Try to fetch tifo data and display the content
+        this.#diagForm.Description.value = tifoData.querySelector("info description").textContent;
+        this.#diagForm.Guid.value = tifoData.getAttribute("guid");
         
-        const VersionNumber = tifoVersions[i].getAttribute("number");
-        let sect = document.createElement("section");
-        let header = document.createElement("h1");
-        let list = document.createElement("ul");
+        // Load Structure Info
+        const StructInfo = tifoData.querySelectorAll("info structure level");
+        this.#numStruct.value = StructInfo.length;
+        this.#SetupStructureLevels();
 
-        header.textContent = `Version ${VersionNumber}`;
+        for (let i = 1; i <= this.#numStruct.value; i++) {
+            document.getElementById(`DiagEditSnipStructLvl${i}`).value = StructInfo[i - 1].textContent;
+        }
 
-        // Iterate all available languages
-        let tifoVersionsLangauges = tifoVersions[i].querySelectorAll("content");
-        for (let j = 0; j < tifoVersionsLangauges.length; j++) {
-            let listEntry = document.createElement("li");
-            let EntryLabel = document.createElement("label");
-            let EntryLabelLang = document.createElement("data");
-            let EntryLabelTranstate = document.createElement("data");
-            let EntryLabelDate = document.createElement("time");
-            let EntryText = document.createElement("textarea");
+        // Load Versions
+        // Cleanup existing Versions, if dialog was opened before
+        const OldVersions = this.#fieldVersion.querySelectorAll("section");
+        for (let i = 0; i < OldVersions.length; i++) {
+            this.#fieldVersion.removeChild(OldVersions[i]);
+        }
 
-            const TranslateLang = tifoVersionsLangauges[j].getAttribute("lang");
-            const TranslateDate = new Date(tifoVersionsLangauges[j].getAttribute("date"));
-            const TranslateState = tifoVersionsLangauges[j].getAttribute("transtate");
-
-            EntryLabelLang.value = TranslateLang;
-            EntryLabelLang.textContent = TranslateLang;
-
-            EntryLabelTranstate.value = TranslateState;
-            switch (TranslateState) {
-                case "original":
-                    EntryLabelTranstate.textContent = "Original";
-                    break;
+        // Iterate all available versions
+        const tifoVersions = tifoData.querySelectorAll("version");
+        for (let i = 0; i < tifoVersions.length; i++) {
             
-                case "pending":
-                    EntryLabelTranstate.textContent = "Übersetzung ausstehend";
-                    break;
+            const VersionNumber = tifoVersions[i].getAttribute("number");
+            let sect = document.createElement("section");
+            let header = document.createElement("h1");
+            let list = document.createElement("ul");
 
-                case "translated":
-                    EntryLabelTranstate.textContent = "Übersetzung";
-                    break;
+            header.textContent = `Version ${VersionNumber}`;
 
-                default:
-                    break;
+            // Iterate all available languages
+            let tifoVersionsLangauges = tifoVersions[i].querySelectorAll("content");
+            for (let j = 0; j < tifoVersionsLangauges.length; j++) {
+                let listEntry = document.createElement("li");
+                let EntryLabel = document.createElement("label");
+                let EntryLabelLang = document.createElement("data");
+                let EntryLabelTranstate = document.createElement("data");
+                let EntryLabelDate = document.createElement("time");
+                let EntryText = document.createElement("textarea");
+
+                const TranslateLang = tifoVersionsLangauges[j].getAttribute("lang");
+                const TranslateDate = new Date(tifoVersionsLangauges[j].getAttribute("date"));
+                const TranslateState = tifoVersionsLangauges[j].getAttribute("transtate");
+
+                EntryLabelLang.value = TranslateLang;
+                EntryLabelLang.textContent = TranslateLang;
+
+                EntryLabelTranstate.value = TranslateState;
+                switch (TranslateState) {
+                    case "original":
+                        EntryLabelTranstate.textContent = "Original";
+                        break;
+                
+                    case "pending":
+                        EntryLabelTranstate.textContent = "Übersetzung ausstehend";
+                        break;
+
+                    case "translated":
+                        EntryLabelTranstate.textContent = "Übersetzung";
+                        break;
+
+                    default:
+                        break;
+                }
+
+                EntryLabelDate.dateTime = tifoVersionsLangauges[j].getAttribute("date");
+                EntryLabelDate.textContent = TranslateDate.toLocaleDateString();
+
+                EntryLabel.htmlFor = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
+                EntryLabel.appendChild(EntryLabelLang);
+                EntryLabel.appendChild(EntryLabelTranstate);
+                EntryLabel.appendChild(EntryLabelDate);
+
+                EntryText.name = `Version${VersionNumber}${TranslateLang}`;
+                EntryText.id = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
+                EntryText.wrap = "soft";
+                EntryText.required = true;
+                EntryText.innerHTML = tifoVersionsLangauges[j].innerHTML;
+
+                if (j > 0) {
+                    // Set readonly for older Versions, if available
+                    EntryText.readOnly = true;
+                }
+
+                listEntry.appendChild(EntryLabel);
+                listEntry.appendChild(EntryText);
+                list.appendChild(listEntry);
             }
 
-            EntryLabelDate.dateTime = tifoVersionsLangauges[j].getAttribute("date");
-            EntryLabelDate.textContent = TranslateDate.toLocaleDateString();
-
-            EntryLabel.htmlFor = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
-            EntryLabel.appendChild(EntryLabelLang);
-            EntryLabel.appendChild(EntryLabelTranstate);
-            EntryLabel.appendChild(EntryLabelDate);
-
-            EntryText.name = `Version${VersionNumber}${TranslateLang}`;
-            EntryText.id = `DiagEditSnipVers${VersionNumber}${TranslateLang}`;
-            EntryText.wrap = "soft";
-            EntryText.required = true;
-            EntryText.innerHTML = tifoVersionsLangauges[j].innerHTML;
-
-            if (j > 0) {
-                // Set readonly for older Versions, if available
-                EntryText.readOnly = true;
-            }
-
-            listEntry.appendChild(EntryLabel);
-            listEntry.appendChild(EntryText);
-            list.appendChild(listEntry);
+            sect.appendChild(header);
+            sect.appendChild(list);
+            this.#fieldVersion.appendChild(sect);
         }
 
-        sect.appendChild(header);
-        sect.appendChild(list);
-        fieldVersion.appendChild(sect);
+        this.#diagSnip.showModal();
     }
-
-    diagSnip.showModal();
 }
 
-// Logic for close dialog button
-btnClose.addEventListener("click", () => {
-    diagForm.reset();
-    diagSnip.close();
-});
-
-// Logic for increasing and decreasing structure levels
-// Increase-Button
-btnIncStruct.addEventListener("click", () => {
-    diagForm.StructLvlCount.stepUp();
-    SetupStructureLevels();
-});
-
-// Decrease-Button
-btnDecStruct.addEventListener("click", () => {
-    if (diagForm.StructLvlCount.value > 0) {
-        diagForm.StructLvlCount.stepDown();
-        SetupStructureLevels();
-    }
-});
-
-// Event-Handler for changing Structure list
-numStruct.addEventListener("change", () => {
-    SetupStructureLevels();
-});
-
-function SetupStructureLevels() {
-    const NewNumberLevels = Number(numStruct.value);
-    const ActualNumberLevels = fieldStruct.querySelectorAll("div.DiagStructLevel").length;
-
-    if (NewNumberLevels > ActualNumberLevels) {
-        // Adding New Structure Levels
-        for (let i = ActualNumberLevels + 1; i <= NewNumberLevels; i++) {
-            let div = document.createElement("div");
-            let label = document.createElement("label");
-            let input = document.createElement("input");
-
-            label.setAttribute("for", `DiagEditSnipStructLvl${i}`);
-            label.textContent = `Ebene ${i}:`;
-
-            input.type = "text";
-            input.name = `StructLvl${i}`;
-            input.id = `DiagEditSnipStructLvl${i}`;
-            input.placeholder = `Name Ebene ${i}...`;
-            input.minLength = "2";
-            input.maxLength = "20";
-            input.required = true;
-
-            div.className = "DiagStructLevel";
-            div.appendChild(label);
-            div.appendChild(input);
-
-            diagForm.querySelector("fieldset:nth-of-type(2)").appendChild(div);
-            
-        }
-    }
-
-    if (NewNumberLevels < ActualNumberLevels) {
-        // Remove last Structure Levels
-        for (let i = ActualNumberLevels; i > NewNumberLevels; i--) {
-            const lstStruct = fieldStruct.querySelector("div.DiagStructLevel:last-of-type");
-            fieldStruct.removeChild(lstStruct);
-        }
-    }
-}
+// Initialize class
+const SnipDiag = new DialogSnippets("DiagEditSnippet");
